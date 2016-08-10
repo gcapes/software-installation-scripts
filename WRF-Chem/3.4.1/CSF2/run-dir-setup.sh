05c40dda0e31081042c5dc60298b96912759fbb9
Gerard Capes <gerardcapes@gmail.com>
2016-08-10 13:03:52 +0100

    Fix typos and errors in run-dir-setup.sh
    After testing for the first time on the CSF
    (I'd written the script on my PC partly from memory)

--- WRF-Chem/3.4.1/CSF2/run-dir-setup.sh
+++ WRF-Chem/3.4.1/CSF2/run-dir-setup.sh
@@ -2,15 +2,18 @@
 # --------
 # This script is designed for setting up multiple identical run directories to test
 # execution speed for different numbers of cores for the same simulation.
-# Create the run directory, then run this script.
 
-# These variables must be set for the script to work:
-# ---------------------------------------------------
-# export WROOT=<WRF installation directory>
-# export rundir=<where you run the job from>
-# Set which namelist file you want to copy
+# Instructions
+# -------------
+# Create the run directory
+# Check/change the exported variables below
+# Set the following variables:
+  # export WROOT=<WRF installation directory> (already set by installation script)
+  # export rundir=<where you run the job from>
+# Run this script.
+
 export namelistdotinput=~/scratch/wrfchem-run/input_data/namelist.input
-export wrfchemi_link_script=~/software_installation_scripts/WRF-Chem/3.4.1/CSF2/wrfchemi_link.sh
+export wrfchemi_link_script=~/software-installation-scripts/WRF-Chem/3.4.1/CSF2/wrfchemi_link.sh
 export metdir=~/scratch/wrfchem-run/input_data # <location of the met_em.d01.* files>
 export wrfchemidir=$metdir
 export wrfinput_d01=$metdir/wrfinput_d01
@@ -18,20 +21,23 @@ export wrfbdy_d01=$metdir/wrfbdy_d01
 export wrflowinp_d01=$metdir/wrflowinp_d01
 
 # These steps are required for all wrf-chem runs:
---------------------------------------------------
-cp $WROOT/run $rundir
+# --------------------------------------------------
+cp $WROOT/run/* $rundir
 rm $rundir/*.exe 
 for exe in $(ls $WROOT/run/*.exe);do ln -s $exe $rundir;done
 
 # These steps depend on the simulation being run:
---------------------------------------------------
+# --------------------------------------------------
 cp $namelistdotinput $rundir
 for file in $(ls $metdir/met_em.d01*);do ln -s $file $rundir;done
-cp $wrfchemidir/wrfchemi_d01* $rundir
+for file in $(ls $metdir/wrfchemi_d01*);do ln -s $file $rundir;done
 
 # wrf.exe wants a file for each hour.
 # Create links to the midnight files for each subsequent hour as required.
+export startdir=$(pwd)
+cd $rundir # Doug's script operates on the current directory
 . $wrfchemi_link_script
+cd $startdir
 
 echo "+:h:0:ccn1,ccn2,ccn3,ccn4,ccn5,ccn6" > $rundir/add_remove_var.txt
 
