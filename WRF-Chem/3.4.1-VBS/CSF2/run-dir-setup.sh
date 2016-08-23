bcbddd06087961c0405bab2dafbc79850b4848e9
Gerard Capes <gerardcapes@gmail.com>
2016-08-23 10:12:21 +0100

    Clarify / remove old comments

--- WRF-Chem/3.4.1-VBS/CSF2/run-dir-setup.sh
+++ WRF-Chem/3.4.1-VBS/CSF2/run-dir-setup.sh
@@ -7,16 +7,18 @@
 # -------------
 # Create the run directory
 # Check/change the exported variables below
+#	The idea is to have the input files in one directory, which is linked to
+#	for each run rather than copying the files to each new run directory.
 # Set the following variables:
-  # export WROOT=<WRF installation directory> (already set by installation script)
+  # export WROOT=<WRF installation directory> (WRF installation script sets this)
   # export rundir=<where you run the job from>
 # Run this script.
 
 export inputroot=~/scratch/wrf-chem-vbs-run/vbs_testcase_files
-export namelistdotinput=$inputroot/namelist_files/namelist.input.setup_cbmz_mosaic_anthro_vbs_megan_emission
+# export namelistdotinput=$inputroot/namelist_files/namelist.input.setup_cbmz_mosaic_anthro_vbs_megan_emission
+export namelistdotinput=$inputroot/namelist_files/namelist.input.5days 
 export wrfchemi_link_script=~/software-installation-scripts/WRF-Chem/3.4.1-VBS/CSF2/wrfchemi_link.sh
 export metdir=$inputroot/met_files # <location of the met_em.d01.* files>
-# export wrfchemidir=$metdir
 export wrfinput_d01=$inputroot/wrfinput_d01
 export wrfbdy_d01=$inputroot/wrfbdy_d01
 export wrflowinp_d01=$inputroot/wrflowinp_d01
@@ -33,10 +35,9 @@ ln -sf $namelistdotinput $rundir/namelist.input
 for file in $(ls $metdir/met_em.d01*);do ln -s $file $rundir;done
 ln -s $inputroot/biogenic_emission_files/wrfbiochemi_d01 $rundir
 
-# Write new script for this whole process. Leave files where they are and link to them.	
-	# wrf.exe wants a wrfchemi file for each hour.
-	# Create links to the midnight files for each subsequent hour as required.
-	. $wrfchemi_link_script
+# wrf.exe wants a wrfchemi file for each hour.
+# Create links to the midnight files for each hour as required.
+. $wrfchemi_link_script
 
 ln -s $wrfinput_d01 $rundir
 ln -s $wrfbdy_d01 $rundir
