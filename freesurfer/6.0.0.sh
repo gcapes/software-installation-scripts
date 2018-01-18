fb0d6de41cbba4cab2e7fbf1d488d02ca8bcef58
Gerard Capes <gerard.capes@manchester.ac.uk>
2018-01-18 14:57:01 +0000

    Add freesurfer v6.0.0
    There were a couple of dependencies missing from Ubuntu 16.04.3.

---
+++ freesurfer/6.0.0.sh
@@ -0,0 +1,15 @@
+# Install FreeSurfer v6.0.0 on Ubuntu 16.04
+# See http://surfer.nmr.mgh.harvard.edu/fswiki/DownloadAndInstall#FreeSurferDownloadandInstall
+
+# Download installer
+cd ~/Downloads
+wget ftp://surfer.nmr.mgh.harvard.edu/pub/dist/freesurfer/6.0.0/freesurfer-Linux-centos6_x86_64-stable-pub-v6.0.0.tar.gz
+tar -C /usr/local -xzvf freesurfer-Linux-centos6_x86_64-stable-pub-v6.0.0.tar.gz
+
+# Set up environment
+export FREESURFER_HOME=/usr/local/freesurfer
+source $FREESURFER_HOME/SetUpFreeSurfer.sh
+
+# Install dependencies 
+sudo apt-get install tcsh
+sudo agt-get install libjpeg62
