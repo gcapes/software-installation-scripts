803927ae60c6f8549c61b5e6beb5e3c9b295af2a
Gerard Capes <gerard.capes@manchester.ac.uk>
2019-03-21 09:12:19 +0000

    Install MATLAB engine for Python

---
+++ python/matlab-engine/install-user-csf3.sh
@@ -0,0 +1,19 @@
+# Install MATLAB engine for python in user's home directory
+
+module load apps/python3/3.6.4/gcc-4.8.5
+module load apps/binapps/matlab/R2018a
+cd $MATLAB_HOME/extern/engines/python
+# The `--prefix=` bit is needed due to a bug on CentOS
+# https://stackoverflow.com/questions/4495120/combine-user-with-prefix-error-with-setup-py-install
+python setup.py install --user --prefix=
+
+# Usage from python is then like this:
+  # >>> import matlab.engine
+  # >>> eng = matlab.engine.start_matlab()
+  # >>> tf = eng.isprime(37)
+  # >>> print(tf)
+  # True
+  # >>> eng.quit()
+ 
+  # Calling user-defined functions requires the .m files to be in the current directory.
+  # >>> output = eng.myfunction(in1, in2)
