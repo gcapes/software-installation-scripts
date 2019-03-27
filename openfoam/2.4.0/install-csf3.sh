cc37518ef267d98b5069171064197877b2c2da4b
Gerard Capes <gerard.capes@manchester.ac.uk>
2019-03-27 09:21:44 +0000

    Correct OpenFOAM 2.3.0 and 2.4.0 installers
    Boost and CGAL locations need to be set in the ThirdParty
    CGAL config file for early versions of OpenFOAM.

--- openfoam/2.4.0/install-csf3.sh
+++ openfoam/2.4.0/install-csf3.sh
@@ -1,28 +1,40 @@
 module load compilers/gcc/6.4.0
-module load libs/boost/1.55.0
+module load mpi/gcc/openmpi/3.1.3
+module load libs/gcc/boost/1.68.0
 module load libs/gcc/cgal/4.9
 module load tools/env/proxy
-# OpenMPI is loaded with the boost module on CSF3
 
-FVER=2.4.0
-OF_ROOT=/opt/apps/apps/gcc/openfoam/${FVER}
-mkdir -p ${OF_ROOT}
-cd ${OF_ROOT}
-wget http://downloads.sourceforge.net/foam/OpenFOAM-${FVER}.tgz
-wget http://downloads.sourceforge.net/foam/ThirdParty-${FVER}.tgz
-tar -xzf OpenFOAM-${FVER}.tgz
-tar -xzf ThirdParty-${FVER}.tgz
-FOAM_RC=${OF_ROOT}/OpenFOAM-${FVER}/etc/bashrc
-FOAM_INST_DIR=${OF_ROOT}
-CGAL_ARCH_PATH=$CGAL_DIR
-BOOST_ARCH_PATH=${BOOST_ROOT}
+OF_VER=2.4.0
+OF_DIR=/opt/apps/apps/gcc/openfoam/${OF_VER}
+ETC_DIR=${OF_DIR}/OpenFOAM-${OF_VER}/etc
+FOAM_RC=${ETC_DIR}/bashrc
+
+# Override default installation directory
+FOAM_INST_DIR=${OF_DIR}
+
+mkdir -p ${OF_DIR}
+cd ${OF_DIR}
+wget http://downloads.sourceforge.net/foam/OpenFOAM-${OF_VER}.tgz
+wget http://downloads.sourceforge.net/foam/ThirdParty-${OF_VER}.tgz
+tar -xzf OpenFOAM-${OF_VER}.tgz
+tar -xzf ThirdParty-${OF_VER}.tgz
+# We need to:
+#	export CGAL_ARCH_PATH=$CGAL_DIR
+#	export BOOST_ARCH_PATH=$BOOST_ROOT
+# but old versions of OpenFOAM ignore existing values for BOOST_ARCH_PATH and CGAL_ARCH_PATH
+# and set their default values when sourcing the bashrc file.
+# So we edit the CGAL config file.
+CGAL_CONFIG=${ETC_DIR}/config/CGAL.sh
+sed -i 's/BOOST_ARCH_PATH=.*/BOOST_ARCH_PATH=\$BOOST_ROOT/' $CGAL_CONFIG
+sed -i 's/CGAL_ARCH_PATH=.*/CGAL_ARCH_PATH=\$CGAL_DIR/' $CGAL_CONFIG
 . ${FOAM_RC}
-cd ${OF_ROOT}/ThirdParty-${FVER}
+
+cd ${OF_DIR}/ThirdParty-${OF_VER}
 ./Allwmake 2>&1 | tee allwmake.log
-cd ${OF_ROOT}/ThirdParty-${FVER}/scotch_6.0.0/src
+cd ${OF_DIR}/ThirdParty-${OF_VER}/scotch_6.0.0/src
 make 2>&1 | tee make.log
 cd $WM_PROJECT_DIR
 ./Allwmake 2>&1 | tee allwmake.log
 
 # FOAM_INST_DIR should also be set in the module file, because early versions of OpenFOAM
-# don't detect the installation directory when you source ${OF_ROOT}/etc/bashrc.
+# don't detect the installation directory when you source $FOAM_RC.
