c0017964580d5e04c47eec04127c5b81c66c0ca2
Gerard Capes <gerard.capes@manchester.ac.uk>
2019-05-13 14:57:03 +0100

    Add MATLAB 2019a installer for macOS

---
+++ matlab/R2019a/mac/install.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+
+# Installation script for MATLAB R2019a at the University of Manchester.
+# Instructions:
+	# Download the three iso files, and copy the contents of all three into a new directory.
+	# `cd` to that directory, which should contain:
+	# install.sh common/ examples/ R2019a/
+	# Run this script using `sudo ./install.sh`.
+
+
+# Recombine installation files from the two .iso files
+MVER=R2019a
+INSTALLERDIR="${PWD}"/${MVER}
+if [ ! -d "${INSTALLERDIR}"/archives/common ]; then
+	# Common directory hasn't been copied.
+	if [ -d common ]; then
+		# Common dir exists at same level, so move it to the right place.
+		mv common "${INSTALLERDIR}"/archives
+	else
+		# User error.
+		echo -------- User error! --------
+		echo \"common\" directory missing from current directory. Have you extracted all three iso files?
+		echo Refer back to installation instructions:
+		echo "https://www.applications.itservices.manchester.ac.uk/show_product.php?id=98&tab=install"
+		exit
+	fi
+fi
+
+if [ ! -d "${INSTALLERDIR}"/archives/common/examples ]; then
+	# Examples directory hasn't been copied.
+	if [ -d examples ]; then
+	# Examples dir exists at same level, so move it to the right place.
+		mv examples "${INSTALLERDIR}"/archives/common
+	else
+		# User error.
+		echo -------- User error! --------
+		echo \"examples\" directory missing from current directory. Have you extracted all three iso files?
+		echo Refer back to installation instructions:
+		echo "https://www.applications.itservices.manchester.ac.uk/show_product.php?id=98&tab=install"
+		exit
+	fi
+fi
+
+# Copy licence file to installation directory.
+# Note US spelling of licences!
+export LICENCEDIR=/Applications/MATLAB_${MVER}.app/licenses
+sudo mkdir -p ${LICENCEDIR}
+sudo cp "${INSTALLERDIR}"/network.lic ${LICENCEDIR}
+
+# Run the installer
+sudo "${INSTALLERDIR}"/install -inputFile "${INSTALLERDIR}"/installer_input.txt
