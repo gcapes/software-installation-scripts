8c7c353ccb73ccae19ca84ed4887841b554549af
Gerard Capes <gerard.capes@manchester.ac.uk>
2020-08-11 13:15:38 +0100

    Download installers to install directory

--- matlab/toolboxes/mpt3/install.sh
+++ matlab/toolboxes/mpt3/install.sh
@@ -1,3 +1,7 @@
+INSTALL_DIR=/opt/apps/apps/binapps/matlab/third-party-toolboxes/mpt3/3.2.1
+mkdir -p ${INSTALL_DIR}
+cd ${INSTALL_DIR}
+
 wget http://people.ee.ethz.ch/~mpt/tbx/pool/mpt3/3_2_1/mpt3-3_2_1.tgz
 wget http://people.ee.ethz.ch/~mpt/tbx/pool/mpt3doc/3_0_4/mpt3doc-3_0_4.tgz
 wget http://people.ee.ethz.ch/~mpt/tbx/pool/cddmex/1_0_1/cddmex_1_0_1_GLNXA64.tgz
@@ -8,9 +12,6 @@ wget http://people.ee.ethz.ch/~mpt/tbx/pool/lcp/1_0_3/lcp_1_0_3_GLNXA64.tgz
 wget http://people.ee.ethz.ch/~mpt/tbx/pool/sedumi/1_3/sedumi_1_3_glnxa64.zip
 wget http://people.ee.ethz.ch/~mpt/tbx/pool/espresso/1_0/espresso_linux.tgz
 wget https://github.com/yalmip/YALMIP/archive/master.zip
-
-INSTALL_DIR=/opt/apps/apps/binapps/matlab/third-party-toolboxes/mpt3/3.2.1
-mkdir -p ${INSTALL_DIR}
 
 for file in *.zip
 do
